f0cff2eb4b210f0f4b24167f2d2bfd89a324bd8f
Wouter Heyvaert <wouterheyvaert1@gmail.com>
2026-09-14 09:20:28 +0100

    add basic mini plugins

--- init.lua
+++ init.lua
@@ -2,4 +2,10 @@ vim.pack.add({
   { src = 'https://github.com/nvim-mini/mini.nvim', version = 'stable' },
 })
 
-require('mini.basics').setup({})
+-- mini.basics must be guaranteed to be loaded first as it also sets <leader>
+require('mini.basics').setup({
+  mappings = {
+    windows = true, -- Window navigation with <C-hjkl>, resize with <C-arrow>
+  },
+})
+
